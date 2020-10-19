ba1ccb8dd076f3729be898e413bcb63bce6b414c
Ayush <ashekhar.as@gmail.com>
2020-10-19 18:57:36 +0530

    initial commit

---
+++ precache-manifest.680029088be4daa919d501b78bde0aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af72b1fd89a609287b4f70808ee4870",
+    "url": "/clone-projects/index.html"
+  },
+  {
+    "revision": "2c1bef1da726f3792c52",
+    "url": "/clone-projects/static/css/main.5845c439.chunk.css"
+  },
+  {
+    "revision": "afd4034603387f63df0e",
+    "url": "/clone-projects/static/js/2.cdb35c68.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/clone-projects/static/js/2.cdb35c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1bef1da726f3792c52",
+    "url": "/clone-projects/static/js/main.60a2d1b1.chunk.js"
+  },
+  {
+    "revision": "f65d612cea564b9ed2ee",
+    "url": "/clone-projects/static/js/runtime-main.bf2c39ef.js"
+  }
+]);
